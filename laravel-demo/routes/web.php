2a109ddeaa41827519c6d76d8bb03043e9f76841
phamduythe <thepham3603@gmail.com>
2024-04-03 19:39:09 +0700

    push register

--- laravel-demo/routes/web.php
+++ laravel-demo/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//regis
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
+//
