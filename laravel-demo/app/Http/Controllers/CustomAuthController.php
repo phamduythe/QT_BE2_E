1fd9a26d581007d5633737f4d6641ca3c2a1dc96
KOK97 <phatsucvat0907@gmail.com>
2024-04-11 22:23:17 +0700

    hung

---
+++ laravel-demo/app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+//Unknow
+class CustomAuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email', 'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
+            'password' => 'required|min:6',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('list')->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withErrors(['error' => 'Login details are not valid']);
+    }
+
+    public function dashboard()
+    {
+        if (Auth::check()) {
+            return view('dashboard');
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
+    public function signOut()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
--- laravel-demo/routes/web.php
+++ laravel-demo/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('dashboard', [CustomAuthController::class, 'dashboard'])->name('dashboard');
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
