fa028dcd587bfcd3ae8bf3b06ac9f4d54c4ee386
phamduythe <thepham3603@gmail.com>
2024-04-11 22:03:40 +0700

    push router

--- laravel-demo/routes/web.php
+++ laravel-demo/routes/web.php
@@ -3,31 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomAuthController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-<<<<<<< HEAD
+
 
 //list user
 Route::get('list', [CustomAuthController::class, 'list'])->name('list.user');
 //view user
-<<<<<<< HEAD
+
 Route::get('viewprofie/id{id}', [CustomAuthController::class, 'view'])->name('viewprofie');
 //destroy, update
 Route::get('destroy/{id}', [CustomAuthController::class, 'destroy'])->name('destroy');
 Route::get('update-user/{id}', [CustomAuthController::class, 'update'])->name('update');
 Route::post('update-user/{id}', [CustomAuthController::class, 'customUpdate'])->name('update.custom');
-<<<<<<< HEAD
->>>>>>> khiem
-=======
+
+
 Route::get('viewprofie/id{id}', [CustomAuthController::class, 'view'])->name('viewprofie');
->>>>>>> parent of 1112c84 (Merge branch 'the')
-=======
->>>>>>> origin/master
-=======
+
 //registration
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
 //
->>>>>>> the
+
