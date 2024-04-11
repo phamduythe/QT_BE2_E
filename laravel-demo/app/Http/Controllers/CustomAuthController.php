bcbc72aef9bcaf0add7891b4a00539757ecf220e
khiem <phamvankhiemvx2003@gmail.com>
2024-04-11 21:28:46 +0700

    khiem

--- laravel-demo/app/Http/Controllers/CustomAuthController.php
+++ laravel-demo/app/Http/Controllers/CustomAuthController.php
@@ -12,65 +12,8 @@ use Illuminate\Support\Facades\Storage;
 //Unknow
 class CustomAuthController extends Controller
 {
-    public function index()
-    {
-        return view('auth.login');
-    }
-
-    public function customLogin(Request $request)
-    {
-        $request->validate([
-            'email' => 'required|email', 'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
-            'password' => 'required|min:6',
-        ]);
-
-        $credentials = $request->only('email', 'password');
-        if (Auth::attempt($credentials)) {
-            return redirect()->intended('list')->withSuccess('Signed in');
-        }
-
-        return redirect("login")->withErrors(['error' => 'Login details are not valid']);
-    }
-
-    public function registration()
-    {
-        return view('auth.registration');
-    }
-
-    public function customRegistration(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|unique:users',
-            'email' => 'required|email|unique:users', 'regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
-            'password' => 'required|min:6',
-            'image' => 'nullable', 'image', 'mimes:jpeg,png,jpg,gif','max:2048',
-            'phone' => 'required|min:10', 'regex:/^\d{10}$/',
-        ]);
-
-        $data = $request->all();
-        $check = $this->create($data);
-
-        return redirect("dashboard")->withSuccess('You have register!');
-    }
-
-    public function create(array $data)
-    {
-        if(isset($data['image'])) {
-            $file = $data['image'];
-            $path = 'images/avatar';
-            $fileName = $file->getClientOriginalName();
-            $file->move($path, $fileName); 
-        } else {
-            $fileName = 'avatar_defaul.jpg';
-        }
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'phone' => $data['phone'],
-            'image' =>  $fileName,
-            'password' => Hash::make($data['password'])
-        ])->with('message', 'Người dùng đã được xóa thành công.');
-    }
+    
+   
 
     public function destroy($id)
     {
@@ -123,36 +66,5 @@ class CustomAuthController extends Controller
         return redirect("list")->with('success','You have signed-in');
         
     }
-    public function view($id)
-    {
-        $userData = User::find($id);
-
-        if (!$userData) {
-            view('auth.list');
-        }
-        return view('auth.profile', compact('userData'));
-    }
-
-    public function dashboard()
-    {
-        if (Auth::check()) {
-            return view('dashboard');
-        }
-
-        return redirect("login")->withSuccess('You are not allowed to access');
-    }
-
-    public function signOut()
-    {
-        Session::flush();
-        Auth::logout();
-
-        return Redirect('login');
-    }
-
-    public function list()
-    {
-        $users = User::paginate(1);
-        return view('auth.list', compact('users'));
-    }
+   
 }
--- laravel-demo/routes/web.php
+++ laravel-demo/routes/web.php
@@ -7,24 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('dashboard', [CustomAuthController::class, 'dashboard'])->name('dashboard');
-Route::get('login', [CustomAuthController::class, 'index'])->name('login');
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
-
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
-
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
 
 
-//list user
-Route::get('list', [CustomAuthController::class, 'list'])->name('list.user');
-//add user
-Route::get('add-user', [CustomAuthController::class, 'create'])->name('add.user');
-//destroy user
+
+
 Route::get('destroy/{id}', [CustomAuthController::class, 'destroy'])->name('destroy');
-//update user
 Route::get('update-user/{id}', [CustomAuthController::class, 'update'])->name('update');
 Route::post('update-user/{id}', [CustomAuthController::class, 'customUpdate'])->name('update.custom');
-//view user
-Route::get('viewprofie/id{id}', [CustomAuthController::class, 'view'])->name('viewprofie');
